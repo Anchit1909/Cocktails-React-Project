145eb57fc15b0c9da8b6eef73ee0140b435529e7
Anchit Sinha <anchit1909@gmail.com>
2022-05-17 12:50:08 +0530

    Complete Cocktails Project

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos mollitia
+        perferendis sequi. Quis incidunt provident, repudiandae minima non ut
+        earum quod deleniti, esse, sequi iure. Quis sapiente praesentium neque
+        nisi atque dolorem natus ipsum ipsa dolor. Pariatur voluptates
+        reiciendis soluta!
+      </p>
+    </section>
+  );
+};
+
+export default About;
